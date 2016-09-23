c5434bfb8e197ede9251563111de4348b764bb9d
sofken <sofken@SENKOKA-PC>
2016-09-23 17:57:19 +0900

    sanmoku

---
+++ test/test/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace test
+{
+    class Program
+    {
+
+        static void Main(string[] args)
+        {
+            int[][] field = new int[3][];
+            
+            for (int i = 0; i < 3; i++)
+            {
+                for (int j = 0; j < 3; j++)
+                {
+                    if (field[i][j] == 1) Console.WriteLine("○");
+                    else if (field[i][j] == 2) Console.WriteLine("×");
+                    else if (field[i][j] == 0) Console.Write("{0}{1} ", i, j);
+                }
+                Console.WriteLine();
+            }
+        }
+
+        static int B(int a)
+        {
+            return a * a;
+        }
+    }
+}
